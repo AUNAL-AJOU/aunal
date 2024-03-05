7636e14db1b96cd6787760844829351bad0efebe
AUNAL-AJOU <128766883+AUNAL-AJOU@users.noreply.github.com>
2024-03-05 21:46:47 +0900

    Update people.js

--- src/pages/people.js
+++ src/pages/people.js
@@ -34,11 +34,6 @@ const PeoplePage = ({
       <div className="grids small" style={{ marginBottom: "32px" }}>
         {currentPeople}
       </div>
-      // <h1>Alunmi</h1>
-      // <div className="grids small" style={{ marginBottom: "32px" }}>
-      //   {currentPeople}
-      // </div>
-      
     </Layout>
   );
 };
